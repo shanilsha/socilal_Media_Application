ead625d9dacbac8acfef7509bb2f35888b196467
shanil <Muhammedshanilizza@gmail.com>
2021-06-17 20:45:48 +0530

    first command

---
+++ Social_Media_Application/client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>hello</h1>
+     <h2>world</h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ Social_Media_Application/server/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/post.js'
+
+const app = express();
+
+app.use('/posts',postRoutes);
+
+
+app.use(bodyParser.json({  extended: true }));
+app.use(bodyParser.urlencoded({ extended:true }));
+app.use(cors());
+
+
+const CONNECTION_URL = 'mongodb+srv://login:axsDGoFxLYdFy9qU@cluster0.zate4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL,{ useNewUrlParser:true, useUnifiedTopology:true })
+.then(()=> app.listen(PORT,()=> console.log(`server running on port: ${PORT}`)))
+.catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify',false);
+
